9017c93f530ae943ed9885e7f262c4f23ce7e7ae
Johan Beyers <jbeyers@juizi.com>
2011-07-30 15:13:52 +0200

    Basic paster project

---
+++ fullmarks/theme/browser/interfaces.py
@@ -0,0 +1,9 @@
+from plone.theme.interfaces import IDefaultPloneLayer
+
+
+class IThemeSpecific(IDefaultPloneLayer):
+    """Marker interface that defines a Zope 3 browser layer.
+       If you need to register a viewlet only for the
+       "fullmarks_theme" theme, this interface must be its layer
+       (in theme/viewlets/configure.zcml).
+    """
